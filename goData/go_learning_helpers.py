028296410094ecfc67e4f80d620f8fa31b1ae288
Isaac Robson <isrobson@users.noreply.github.com>
2018-10-22 18:43:37 -0400

    add an n_run validator for a clf and metrics

--- goData/go_learning_helpers.py
+++ goData/go_learning_helpers.py
@@ -7,6 +7,8 @@ import sklearn
 from sklearn.preprocessing import LabelBinarizer as LB
 from sklearn.model_selection import train_test_split as tts
 import sklearn.metrics as skm
+from sklearn.base import clone
+import multiprocessing as mp
 
 def get_go_annotations_series(path_to_file):
     ''' Reads a csv file of {Kinase: "[Annotations]"} and returns a formatted pd.Series
@@ -68,14 +70,15 @@ def add_cluster_labels(path_to_file, ser):
     # may be good to add a NaN checker later
     return dat
 
-def get_tts(dat, test_size=0.2, random_state=None):
+def get_tts(dat, X_col_name='GO Labels', test_size=0.2, random_state=None):
     ''' Gets the train_test_split on dat
     inputs: dat -- DataFrame of kinases with cluster number and GO Labels attached, as output by add_cluster_labels
+            X_col_name -- name of the column(s) to use for the predictor variables
             test_size -- portion of data to place in the test set
             random_state -- random seed used for train_test_split
     '''
     # split into training & testing
-    X_train, X_test, y_train, y_test = tts(dat['GO Labels'], dat['cluster'], test_size=test_size, random_state=random_state)
+    X_train, X_test, y_train, y_test = tts(dat[X_col_name], dat['cluster'], test_size=test_size, random_state=random_state)
 
     # convert to sklearn-friendly format (lists of arrays)
     X_train = X_train.values.tolist()
@@ -112,3 +115,54 @@ def score_model(y_true, y_predict, metric='accuracy', kwargs={}):
     score = scorer(y_true, y_predict, **kwargs)
 
     return score
+
+def validate_learnability(n_run, dat_df, clf, X_col_name='GO Labels', test_size = 0.3, parallel=None, metrics=['accuracy'], scorer_kwargs={}):
+    ''' takes the output of an add_cluster_labels and calls clf.fit(), get_tts, and score_model n_run times
+    inputs: dat_df -- the input dat with 'cluster' response and X_col_name predictor
+            clf -- a classifier with .fit() and .predict() methods
+            X_col_name -- a string (or list of strings) corresponding to the predictors in dat_df
+            test_size -- proportion of dat_df to use in the holdout sets
+            parallel -- how many cores to use, default None is sequential operation on a single core
+            metric -- the metric to call for score_model
+    output: out -- an array of length n_run of scores from score_model on each of the holdout sets
+    '''
+    num_metrics = len(metrics)
+
+    if parallel is None:
+        clf_local = clone(clf) # clone original clf for parallel read/write safety
+        output = []
+        for i in range(n_run):
+            # split, fit, score, append
+            X_train, X_test, y_train, y_test = get_tts(dat_df, X_col_name=X_col_name, test_size=test_size)
+            clf.fit(X_train, y_train)
+
+            # loop through the metrics
+            scores = []
+            for i in range(num_metrics):
+                scores.append(score_model(y_test, clf.predict(X_test), metric=metrics[i], kwargs=scorer_kwargs))
+
+            # append a tuple if more than one metric, else just a float
+            if(num_metrics > 1):
+                output.append(tuple(scores))
+            else:
+                output.append(scores[0])
+    else:
+        n_run_array = [n_run//parallel]*parallel # number of runs per core
+
+        # add one if needed to some cores
+        overflow = n_run % parallel
+        if overflow > 0:
+            n_run_array[:overflow] = n_run_array[:overflow] + [1]*overflow
+
+        # create a handy kwarg dict --- note that parallel is None, so we run sequentially in parallel
+        kwarg_dict = {'dat_df':dat_df, 'clf':clf, 'X_col_name':X_col_name, 'test_size':test_size, 'parallel':None, 'metrics':metrics, 'scorer_kwargs':scorer_kwargs}
+        val_learn = ft.partial(validate_learnability, **kwarg_dict)
+
+        # create a pool, map the run values
+        p = mp.Pool(parallel)
+        output = p.map(val_learn, n_run_array)
+        p.close()
+
+        output = list(it.chain.from_iterable(output))
+
+    return output
