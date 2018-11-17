b1b47714d7b7a6774dce5e816fd2830d141c8ffd
Isaac Robson <isrobson@email.unc.edu>
2018-11-16 19:39:07 -0500

    update split with temporary Louvain

--- src/SplitLargeSubnet.R
+++ src/SplitLargeSubnet.R
@@ -3,28 +3,55 @@
 #install.packages("ProNet")
 #install.packages("igraph")
 library("igraph")
-library("ProNet")
+#library("ProNet")
 
 #G <- read.graph("/Users/lutzka/Desktop/testgraph.txt",format="ncol",names=TRUE,weights="yes",directed=FALSE)
 #G <- read.graph("/Users/lutzka/Desktop/Kinome+OneHopCompiled/All_Databases_Combined_Gene_Names_one_hop_to_kinases.txt",format="ncol",names=TRUE,weights="no",directed=FALSE)
-G <- read.graph("/Users/lutzka/Dropbox/Analyses/DrugComboMethod/Data/GenericKinome/Full_Kinome_Network_Compiled_weighted_pathways.txt",format="ncol",names=TRUE,weights="yes",directed=FALSE)
+G <- read.graph("~/Github/KIN_ClusteringWithAnnotations/data/Full_Kinome_Network_Compiled_weighted_pathways.txt",format="ncol",names=TRUE,weights="yes",directed=FALSE)
 #G <- read.graph(args[1],format="ncol",names=TRUE,weights=args[2],directed=FALSE)
 #dd <- read.table("/Users/lutzka/Dropbox/GomezLabShare/Kinome/NetworkData/All_Databases_Combined_Gene_Names_kinome_only.txt")
 #G <- graph.data.frame(dd,directed=FALSE)
-tmp <- read.table("/Users/lutzka/Dropbox/Analyses/DrugComboMethod/Data/GenericKinome/Full_Kinome_Network_Compiled_weighted_pathways.txt")
+tmp <- read.table("~/Github/KIN_ClusteringWithAnnotations/data/Full_Kinome_Network_Compiled_weighted_pathways.txt")
 W <- tmp$V3
 
-outfile="/Users/lutzka/Dropbox/Analyses/DrugComboMethod/Results/Kinase_all_clustering_membership_results.txt"
-
+  
+outfile="~/Github/KIN_ClusteringWithAnnotations/results/consensus_spinglass.txt"
 #Analyze largest CSpG cluster to split
 tmp <- read.table(outfile,header=T)
-freq <- table(tmp$SpinGlassCommunity)
+freq <- table(tmp$cluster)
 x <- as.numeric(names(freq)[which.max(freq)])
-lgclust <- which(tmp$SpinGlassCommunity == x)
-
+lgclust <- which(tmp$cluster == x)
 subG <- induced_subgraph(G, lgclust, impl="auto")
 
 ###2. spinglass.community
+louv <- cluster_louvain(subG)
+
+louv$memberships
+louv$membership
+full_louv <- cluster_louvain(G)
+full_louv$memberships[1,]
+full_louv$memberships[2,]
+full_louv$names
+louv_child_clusters <- data.frame(names=full_louv$names, cluster=full_louv$memberships[1,])
+
+freq <- table(louv_child_clusters$cluster)
+x <- as.numeric(names(freq)[which.max(freq)])
+lgclust <- which(louv_child_clusters$cluster == x)
+subG <- induced_subgraph(G, lgclust, impl="auto")
+
+
+write.table(louv_child_clusters, '~/Github/KIN_ClusteringWithAnnotations/results/louvain_small_clusters.txt', quote=FALSE,sep="\t",row.names=FALSE)
+modularity(G, membership=full_louv$memberships[1,], weights = W)
+modularity(G, membership=sc$cluster+1, weights = W)
+sc <- read.table('~/Github/KIN_ClusteringWithAnnotations/results/consensus_spinglass.txt', header=T)
+
+out <- list()
+for (i in 1:length(unique(full_louv$memberships[1,]))){
+  c <- unique(full_louv$memberships[1,])[i]
+  out[i] = length(full_louv$memberships[2,][which(full_louv$memberships[1,]==c)])
+}
+sum(as.numeric(out))
+max(as.numeric(out))
 sc <- spinglass.community(subG, spins=100)
 
 numnodes <- length(sc$names)
