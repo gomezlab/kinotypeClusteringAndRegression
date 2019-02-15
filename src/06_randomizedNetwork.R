48c6e6c756c672166aed2e403d5d2dabccee9d4a
Isaac Robson <isrobson@users.noreply.github.com>
2019-02-15 04:26:55 -0500

    fix pre-run folder

--- src/06_randomizedNetwork.R
+++ src/06_randomizedNetwork.R
@@ -36,7 +36,7 @@ write.graph(mainG, '~/Github/KIN_ClusteringWithAnnotations/data/kin/kin_random_g
 ###1. fastgreedy.community
 fc <- fastgreedy.community(mainG)
 fg_clusts <- data.frame(names=fc$names, cluster=fc$membership)
-write.table(fg_clusts, '~/GitHub/KIN_ClusteringWithAnnotations/randomizedResults/fastgreedy_clusters.txt',quote=FALSE,sep="\t",row.names=FALSE)
+write.table(fg_clusts, '~/GitHub/KIN_ClusteringWithAnnotations/results/randomizedResults/fastgreedy_clusters.txt',quote=FALSE,sep="\t",row.names=FALSE)
 
 ###2. spinglass.community - knows to use in weights
 sc <- spinglass.community(mainG, spins=100)
@@ -64,7 +64,7 @@ for (i in 1:numnodes){
 }
 
 sc_clusts <- data.frame(names=sc$names, cluster=groups)
-write.table(sc_clusts, '~/Github/KIN_ClusteringWithAnnotations/randomizedResults/consensus_spinglass.txt',quote=FALSE,sep="\t",row.names=FALSE)
+write.table(sc_clusts, '~/Github/KIN_ClusteringWithAnnotations/results/randomizedResults/consensus_spinglass.txt',quote=FALSE,sep="\t",row.names=FALSE)
 
 ###3. leading.eigenvector.community -- knows to use weights
 lev <- leading.eigenvector.community(mainG)
@@ -90,7 +90,7 @@ for (i in 1:numnodes){
 }
 
 lev_clusts <- data.frame(names=lev$names, cluster=groups)
-write.table(lev_clusts, '~/Github/KIN_ClusteringWithAnnotations/randomizedResults/consensus_eigenvector.txt',quote=FALSE,sep="\t",row.names=FALSE)
+write.table(lev_clusts, '~/Github/KIN_ClusteringWithAnnotations/results/randomizedResults/consensus_eigenvector.txt',quote=FALSE,sep="\t",row.names=FALSE)
 
 ###4. label.propagation.community
 lp <- label.propagation.community(mainG)
@@ -116,7 +116,7 @@ for (i in 1:numnodes){
 }
 
 lp_clusts <- data.frame(names=lp$names, cluster=groups)
-write.table(lp_clusts, '~/GitHub/KIN_ClusteringWithAnnotations/randomizedResults/consensus_label_propagation.txt',quote=FALSE,sep="\t",row.names=FALSE)
+write.table(lp_clusts, '~/GitHub/KIN_ClusteringWithAnnotations/results/randomizedResults/consensus_label_propagation.txt',quote=FALSE,sep="\t",row.names=FALSE)
 
 ###5. walktrap.community
 wt <- walktrap.community(mainG, modularity=TRUE)
@@ -142,14 +142,14 @@ for (i in 1:numnodes){
 }
 
 wt_clusts <- data.frame(names=wt$names, cluster=groups)
-write.table(wt_clusts, '~/Github/KIN_ClusteringWithAnnotations/randomizedResults/consensus_walktrap.txt',quote=FALSE,sep="\t",row.names=FALSE)
+write.table(wt_clusts, '~/Github/KIN_ClusteringWithAnnotations/results/randomizedResults/consensus_walktrap.txt',quote=FALSE,sep="\t",row.names=FALSE)
 
 ###6. cluster_louvain
 louv <- cluster_louvain(mainG)
 louv_clusts <- data.frame(names=louv$names, cluster=louv$memberships[2,])
 louv_small_clusts <- data.frame(names=louv$names, cluster=louv$memberships[1,])
-write.table(louv_clusts, '~/GitHub/KIN_ClusteringWithAnnotations/randomizedResults/louvain_clusters.txt',quote=FALSE,sep="\t",row.names=FALSE)
-write.table(louv_small_clusts, '~/GitHub/KIN_ClusteringWithAnnotations/randomizedResults/louvain_small_clusters.txt',quote=FALSE,sep="\t",row.names=FALSE)
+write.table(louv_clusts, '~/GitHub/KIN_ClusteringWithAnnotations/results/randomizedResults/louvain_clusters.txt',quote=FALSE,sep="\t",row.names=FALSE)
+write.table(louv_small_clusts, '~/GitHub/KIN_ClusteringWithAnnotations/results/randomizedResults/louvain_small_clusters.txt',quote=FALSE,sep="\t",row.names=FALSE)
 
 ###7. cluster_infomap (random walks)
 info <- walktrap.community(mainG)
@@ -175,12 +175,12 @@ for (i in 1:numnodes){
 }
 
 info_clusts <- data.frame(names=info$names, cluster=groups)
-write.table(info_clusts, '~/Github/KIN_ClusteringWithAnnotations/randomizedResults/consensus_infomap.txt',quote=FALSE,sep="\t",row.names=FALSE)
+write.table(info_clusts, '~/Github/KIN_ClusteringWithAnnotations/results/randomizedResults/consensus_infomap.txt',quote=FALSE,sep="\t",row.names=FALSE)
 
 ###8. edge.betweenness.community
 eb <- edge.betweenness.community(mainG)
 eb_clusts <- data.frame(names=eb$names, cluster=eb$membership)
-write.table(eb_clusts, '~/GitHub/KIN_ClusteringWithAnnotations/randomizedResults/edge_betweenness_community_clusters.txt',quote=FALSE,sep="\t",row.names=FALSE)
+write.table(eb_clusts, '~/GitHub/KIN_ClusteringWithAnnotations/results/randomizedResults/edge_betweenness_community_clusters.txt',quote=FALSE,sep="\t",row.names=FALSE)
 
 #### collect modularity data
 mod <- data.frame(row.names = "modularity")
@@ -196,5 +196,5 @@ mod$small_louvain <- modularity(mainG,louv_small_clusts$cluster,weights=W)
 mod$infomap <- modularity(mainG,info_clusts$cluster,weights=W)
 mod$edge_between <- modularity(mainG,eb_clusts$cluster,weights=W)
 
-outfile="~/Github/KIN_ClusteringWithAnnotations/randomizedResults/clustering_modularity_results.txt"
+outfile="~/Github/KIN_ClusteringWithAnnotations/results/randomizedResults/clustering_modularity_results.txt"
 write.table(mod,outfile,quote=FALSE,sep="\t",row.names = FALSE)
