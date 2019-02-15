666edec91ce06c04200e72c7fa80e5dd69ba57e1
Isaac Robson <isrobson@users.noreply.github.com>
2019-02-15 04:23:48 -0500

    pre-run of 06_randomizedNetwork

--- src/06_randomizedNetwork.R
+++ src/06_randomizedNetwork.R
@@ -1,32 +1,42 @@
-#!/usr/bin/env Rscript
+## do some parallelization
+library('parallel')
+library('purrr')
 library("igraph")
-source("~/GitHub/KIN_ClusteringWithAnnotations/src/tools/communityHelpers.R")
 
-# set seed
-set.seed(1920)
-# set numiter
+## set the number of iterations for stochastic algos
 numiter <- 1000
 
-# read in original graph
-G <- read.graph("~/Github/KIN_ClusteringWithAnnotations/data/kin/kin_anscombe_weighted.csv",format="ncol",names=TRUE,weights="yes",directed=FALSE)
-tmp <- read.table("~/Github/KIN_ClusteringWithAnnotations/data/kin_anscombe_weighted.csv")
-W <- tmp$V3
+# community detection parallelization scripts
+source("~/GitHub/KIN_ClusteringWithAnnotations/src/tools/communityHelpers.R")
 
-#optional -- read existing from file
-#new_G <- read.graph('~/Github/KIN_ClusteringWithAnnotations/data/kin_random_graph.txt',names=TRUE,format='ncol',directed=FALSE)
+# original graph and weights
+G <- read.graph("~/Github/KIN_ClusteringWithAnnotations/data/kin/kin_anscombe_weighted.csv",format="ncol",names=TRUE,weights="yes",directed=FALSE)
+tmp <- read.table("~/Github/KIN_ClusteringWithAnnotations/data/kin/kin_anscombe_weighted.csv")
+W <- tmp$V3
+# reed from file if need be
+#new_G <- read.graph('~/Github/KIN_ClusteringWithAnnotations/data/kin/kin_random_graph.csv',names=TRUE,format='ncol',directed=FALSE)
+
+# compute a new graph
 d <- degree(G)
+set.seed(1920)
 new_G <- degree.sequence.game(out.deg = d, method=c("vl"))
+
 V(new_G)$comp <- components(new_G)$membership
-V(new_G)$name <- V(G)$name
-E(new_G)$weight <- sample(W)
+V(new_G)$name <- as.character(V(G)$name)
+
+# optional -- randomly weight; this doesn't match the distribution of new_G as
+# common KEGG kinases should on average have higher weighted edges
+# E(new_G)$weight <- sample(W)
 
 mainG <- induced_subgraph(new_G,V(new_G)$comp==1)
-write.table(mainG, '~/Github/KIN_ClusteringWithAnnotations/kin/kin_random_graph.txt',quote=FALSE,sep="\t",row.names=FALSE)
+
+# save to file if need be
+write.graph(mainG, '~/Github/KIN_ClusteringWithAnnotations/data/kin/kin_random_graph.csv', format = "ncol")
 
 ###1. fastgreedy.community
 fc <- fastgreedy.community(mainG)
 fg_clusts <- data.frame(names=fc$names, cluster=fc$membership)
-write.table(fg_clusts, '~/GitHub/KIN_ClusteringWithAnnotations/results/randomizedResults/fastgreedy_clusters.txt',quote=FALSE,sep="\t",row.names=FALSE)
+write.table(fg_clusts, '~/GitHub/KIN_ClusteringWithAnnotations/randomizedResults/fastgreedy_clusters.txt',quote=FALSE,sep="\t",row.names=FALSE)
 
 ###2. spinglass.community - knows to use in weights
 sc <- spinglass.community(mainG, spins=100)
@@ -35,10 +45,6 @@ sc <- spinglass.community(mainG, spins=100)
 numnodes <- length(sc$names)
 votes <- mat.or.vec(numnodes,numnodes)
 dim(votes)
-
-## do some parallelization
-library('parallel')
-library('purrr')
 
 print(Sys.time())
 votes <- para_spinglass(g=mainG, numnodes = numnodes, numiter = numiter)
@@ -57,10 +63,8 @@ for (i in 1:numnodes){
   }
 }
 
-sum()
-
 sc_clusts <- data.frame(names=sc$names, cluster=groups)
-write.table(sc_clusts, '~/Github/KIN_ClusteringWithAnnotations/results/randomizedResults/consensus_spinglass.txt',quote=FALSE,sep="\t",row.names=FALSE)
+write.table(sc_clusts, '~/Github/KIN_ClusteringWithAnnotations/randomizedResults/consensus_spinglass.txt',quote=FALSE,sep="\t",row.names=FALSE)
 
 ###3. leading.eigenvector.community -- knows to use weights
 lev <- leading.eigenvector.community(mainG)
@@ -86,7 +90,7 @@ for (i in 1:numnodes){
 }
 
 lev_clusts <- data.frame(names=lev$names, cluster=groups)
-write.table(lev_clusts, '~/Github/KIN_ClusteringWithAnnotations/results/randomizedResults/consensus_eigenvector.txt',quote=FALSE,sep="\t",row.names=FALSE)
+write.table(lev_clusts, '~/Github/KIN_ClusteringWithAnnotations/randomizedResults/consensus_eigenvector.txt',quote=FALSE,sep="\t",row.names=FALSE)
 
 ###4. label.propagation.community
 lp <- label.propagation.community(mainG)
@@ -112,18 +116,17 @@ for (i in 1:numnodes){
 }
 
 lp_clusts <- data.frame(names=lp$names, cluster=groups)
-write.table(lp_clusts, '~/GitHub/KIN_ClusteringWithAnnotations/results/randomizedResults/consensus_label_propagation.txt',quote=FALSE,sep="\t",row.names=FALSE)
+write.table(lp_clusts, '~/GitHub/KIN_ClusteringWithAnnotations/randomizedResults/consensus_label_propagation.txt',quote=FALSE,sep="\t",row.names=FALSE)
 
 ###5. walktrap.community
-wt <- walktrap.community(mainG, modularity=TRUE, steps=gorder(mainG))
+wt <- walktrap.community(mainG, modularity=TRUE)
 
 numnodes <- length(wt$names)
 votes <- mat.or.vec(numnodes,numnodes)
 
 print(Sys.time())
-votes <- para_wt(g=mainG, numnodes = numnodes, numiter = numiter, steps=gorder(mainG))
+votes <- para_wt(g=mainG, numnodes = numnodes, numiter = numiter)
 print(Sys.time())
-
 
 thresh <- 0.9*numiter
 visited <- mat.or.vec(numnodes,1)
@@ -139,16 +142,16 @@ for (i in 1:numnodes){
 }
 
 wt_clusts <- data.frame(names=wt$names, cluster=groups)
-write.table(wt_clusts, '~/Github/KIN_ClusteringWithAnnotations/results/randomizedResults/consensus_walktrap.txt',quote=FALSE,sep="\t",row.names=FALSE)
+write.table(wt_clusts, '~/Github/KIN_ClusteringWithAnnotations/randomizedResults/consensus_walktrap.txt',quote=FALSE,sep="\t",row.names=FALSE)
 
 ###6. cluster_louvain
 louv <- cluster_louvain(mainG)
 louv_clusts <- data.frame(names=louv$names, cluster=louv$memberships[2,])
 louv_small_clusts <- data.frame(names=louv$names, cluster=louv$memberships[1,])
-write.table(louv_clusts, '~/GitHub/KIN_ClusteringWithAnnotations/results/randomizedResults/louvain_clusters.txt',quote=FALSE,sep="\t",row.names=FALSE)
-write.table(louv_small_clusts, '~/GitHub/KIN_ClusteringWithAnnotations/results/randomizedResults/louvain_small_clusters.txt',quote=FALSE,sep="\t",row.names=FALSE)
+write.table(louv_clusts, '~/GitHub/KIN_ClusteringWithAnnotations/randomizedResults/louvain_clusters.txt',quote=FALSE,sep="\t",row.names=FALSE)
+write.table(louv_small_clusts, '~/GitHub/KIN_ClusteringWithAnnotations/randomizedResults/louvain_small_clusters.txt',quote=FALSE,sep="\t",row.names=FALSE)
 
-###7. cluster_infomap (random walks) 
+###7. cluster_infomap (random walks)
 info <- walktrap.community(mainG)
 
 numnodes <- length(info$names)
@@ -172,19 +175,19 @@ for (i in 1:numnodes){
 }
 
 info_clusts <- data.frame(names=info$names, cluster=groups)
-write.table(info_clusts, '~/Github/KIN_ClusteringWithAnnotations/results/randomizedResults/consensus_infomap.txt',quote=FALSE,sep="\t",row.names=FALSE)
+write.table(info_clusts, '~/Github/KIN_ClusteringWithAnnotations/randomizedResults/consensus_infomap.txt',quote=FALSE,sep="\t",row.names=FALSE)
 
 ###8. edge.betweenness.community
 eb <- edge.betweenness.community(mainG)
 eb_clusts <- data.frame(names=eb$names, cluster=eb$membership)
-write.table(eb_clusts, '~/GitHub/KIN_ClusteringWithAnnotations/results/randomizedResults/edge_betweenness_community_clusters.txt',quote=FALSE,sep="\t",row.names=FALSE)
+write.table(eb_clusts, '~/GitHub/KIN_ClusteringWithAnnotations/randomizedResults/edge_betweenness_community_clusters.txt',quote=FALSE,sep="\t",row.names=FALSE)
 
 #### collect modularity data
 mod <- data.frame(row.names = "modularity")
 mod$fast_greedy <- modularity(mainG,fg_clusts$cluster,weights=W)
-# modularity function doesn't accept the '0' cluster name, so we shift all 
+# modularity function doesn't accept the '0' cluster name, so we shift all
 # membership values up by one to get the modularity
-mod$spinglass <- modularity(mainG,sc_clusts$cluster+1,weights=W) 
+mod$spinglass <- modularity(mainG,sc_clusts$cluster+1,weights=W)
 mod$eigen <- modularity(mainG,lev_clusts$cluster,weights=W)
 mod$walktrap <- modularity(mainG,wt_clusts$cluster,weights=W)
 mod$label <- modularity(mainG,lp_clusts$cluster,weights=W)
@@ -193,7 +196,5 @@ mod$small_louvain <- modularity(mainG,louv_small_clusts$cluster,weights=W)
 mod$infomap <- modularity(mainG,info_clusts$cluster,weights=W)
 mod$edge_between <- modularity(mainG,eb_clusts$cluster,weights=W)
 
-outfile="~/Github/KIN_ClusteringWithAnnotations/results/randomizedResults/clustering_modularity_results.txt"
+outfile="~/Github/KIN_ClusteringWithAnnotations/randomizedResults/clustering_modularity_results.txt"
 write.table(mod,outfile,quote=FALSE,sep="\t",row.names = FALSE)
-
-
